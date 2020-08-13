133c05676ab001d0dc30796f73f0b83462d79102
bcy <bcygsws@163.com>
2020-08-13 20:09:11 +0800

    主页tab栏切换2

--- src/components/home/Member.vue
+++ src/components/home/Member.vue
@@ -2,7 +2,14 @@
 </template>
 
 <script>
-    export default {};
+    export default {
+        data() {
+            return {
+                
+            }
+        },
+    };
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+</style>
