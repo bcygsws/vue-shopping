eb6a8687586efffaee452705617ff4d2694c78fb
bcy <bcygsws@163.com>
2020-08-14 22:35:18 +0800

    主页六宫格布局实现

--- src/components/tabbar/Home.vue
+++ src/components/tabbar/Home.vue
@@ -14,7 +14,46 @@
       </mt-swipe-item>
     </mt-swipe> -->
     <swipe :lunbo="lunboList" :isfull="true"></swipe>
-  </div>
+    <!-- 六宫格，又九宫格改造而来 -->
+      <ul class="mui-table-view mui-grid-view mui-grid-9">
+        <li class="mui-table-view-cell mui-media mui-col-xs-4">
+          <a href="#">
+            <img src="../../images/menu1.png" alt="" />
+            <div class="mui-media-body">教育新闻</div>
+          </a>
+        </li>
+        <li class="mui-table-view-cell mui-media mui-col-xs-4">
+          <a href="#">
+            <img src="../../images/menu2.png" alt=""/></img>
+            <div class="mui-media-body">课程分享</div>
+          </a>
+        </li>
+        <li class="mui-table-view-cell mui-media mui-col-xs-4">
+          <a href="#">
+            <img src="../../images/menu3.png" alt="">
+            <div class="mui-media-body">书籍购买</div>
+          </a>
+        </li>
+        <li class="mui-table-view-cell mui-media mui-col-xs-4">
+          <a href="#">
+            <img src="../../images/menu4.png" alt="">
+            <div class="mui-media-body">留言反馈</div>
+          </a>
+        </li>
+        <li class="mui-table-view-cell mui-media mui-col-xs-4">
+          <a href="#">
+            <img src="../../images/menu5.png" alt="">
+            <div class="mui-media-body">视频专区</div>
+          </a>
+        </li>
+        <li class="mui-table-view-cell mui-media mui-col-xs-4">
+          <a href="#">
+            <img src="../../images/menu6.png" alt="">
+            <div class="mui-media-body">联系我们</div>
+          </a>
+        </li>
+      </ul>
+    </div>
 </template>
 
 <script>
@@ -49,4 +88,25 @@ export default {
 };
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.container{
+.mui-grid-view{
+  background-color: #fff;
+    li.mui-media{
+      background-color: #fff;
+      border: 0;
+      a{
+        img{
+          width: 60px;
+          height: 60px;
+          vertical-align: bottom;
+        }
+        .mui-media-body{
+          font-size: 13px;
+        }
+      }
+    }
+  }
+}
+  
+</style>
