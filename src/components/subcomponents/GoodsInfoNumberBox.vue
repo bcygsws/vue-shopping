d597c917e693e3d80f3dc5ce2126ddc0128a5a41
bcy <bcygsws@163.com>
2020-08-25 17:49:24 +0800

    说明文档

--- src/components/subcomponents/GoodsInfoNumberBox.vue
+++ src/components/subcomponents/GoodsInfoNumberBox.vue
@@ -26,12 +26,12 @@
 
 <script>
 // numbox手动初花时，必须再次引入mui.min.js文件,否则报错：mui未定义
-import mui from "../../lib/mui/js/mui.min.js";
+import mui from '../../lib/mui/js/mui.min.js';
 export default {
   // curVal是购物车订单Shopping.vue中传入的当前值
-  props: ["maxVal", "curVal"],
+  props: ['maxVal', 'curVal'],
   mounted() {
-    mui(".mui-numbox").numbox();
+    mui('.mui-numbox').numbox();
     // 获取父组件goodsinfo传递过来的库存量，即数字输入框的最大值
     /* 
         然而，拿到的父组件传过来的值maxVal却是undefined？
@@ -46,14 +46,15 @@ export default {
     */
     console.log(this.maxVal); //undefined
   },
+  // vue官方文档关于watch:大多数情况下我们使用计算属性能计算这种变化。但是，当需要在数据变化时，执行异步操作或者开销较大的请求时，这种方式是有效的。
   watch: {
     //这个maxVal是props中接收的属性
     maxVal: function(newVal, oldVal) {
       //使用mui官方文档中mui的API来设置max的值
       //setOptions(options) 可取值: min(Int),step(Int),max(Int),下面的max是指文本输入框的最大值，这是固定写法，注意和监控属性max的区别
-      mui(".mui-numbox")
+      mui('.mui-numbox')
         .numbox()
-        .setOption("max", newVal);
+        .setOption('max', newVal);
     },
   },
   // 手动初始化mui后，先监听父组件的传值maxVal，并使用mui API动态设置文本输入框最大值。还需要拿到文本输入框中的实时值(某件商品的件数)。
@@ -62,7 +63,7 @@ export default {
     countChanged() {
       // ref---this.$refs可以获取原生DOM对象
       // 当前value值 this.$refs.numbox.value,需要传给父组件
-      this.$emit("getcount", this.$refs.numbox.value);
+      this.$emit('getcount', this.$refs.numbox.value);
     },
   },
 };
